d8296de6b4bcc2ef24c0d2989ab61ca10493d35c
Min Si <msi@is.s.u-tokyo.ac.jp>
2014-02-24 20:04:21 +0900

    複数サブ項目を含んだ文字列をチェックするCHECK_FUNCを追加

--- data/class_extends/SC_CheckError_Ex.php
+++ data/class_extends/SC_CheckError_Ex.php
@@ -24,4 +24,47 @@
 require_once CLASS_REALDIR . 'SC_CheckError.php';
 
 class SC_CheckError_Ex extends SC_CheckError {
+
+    /**
+     * 複数項目を含んだ文字列をチェックする
+     *
+     * @param array $value 各要素は以下の通り。<br>
+     *     [0]: 項目名<br>
+     *     [1]: 判定対象を格納している配列キー
+     *     [2]: 複数サブ文字列を区切る文字列
+     *     [3]: サブ文字列のチェック1
+     *     [4]: サブ文字列のチェック2
+     *     ...
+     * @return void
+     */
+	function MULTIPLE_SUB_CHECK($value) {
+		if (isset($this->arrErr[$value[1]])) {
+			return;
+		}
+
+		$value_param = array($value[0], $value[1]);
+		$checks = array_slice($value, 3);
+		
+		$this->createParam($value_param);
+		$str = $this->arrParam[$value[1]];
+		
+		if(!empty($str) && is_array($checks) && !empty($value["2"])){
+			$subs = split($value["2"], $str);
+
+			// サブ文字列を設定して、チェックする
+			$i = 1;
+			foreach($subs as $s){
+				$s = trim($s);
+				$this->arrParam[$value[1]] = $s;
+				$this->doFunc(array($value[0]."(サブ{$i})", $value[1]), $checks);
+				// エラーが発生したら中止する
+				if (isset($this->arrErr[$value[1]])) {
+					return;
+				}
+				$i++;
+			}
+		}
+		// 値を回復
+		$this->arrParam[$value[1]] = $str;
+	}
 }
